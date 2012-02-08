beb39c2edf49fb1c9ae4e96ecc429d7294fde3fc
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-02-08 15:10:33 +0100

    test for timeouts (a bit nasty, since it actually requires 2 threads...)

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -86,6 +86,57 @@ BOOST_AUTO_TEST_CASE(client_loop){
     boost::asio::io_service io;
     clt.reg(io, 1);
     hub.reg(io, 1);
+
+    boost::asio::deadline_timer dt(io, boost::posix_time::seconds(4));
+    dt.async_wait(boost::bind(&boost::asio::io_service::stop, &io));
+
     io.run();
+
+    BOOST_CHECK_EQUAL(1, hub.get_n_ok());
+}
+
+struct work_forever_client
+: public Client{
+    work_forever_client(std::string a, std::string b): Client(a,b){}
+    bool caught;
+    void handle_task(const mongo::BSONObj& o){
+        caught=false;
+        try{
+            while(true){
+                boost::this_thread::sleep(boost::posix_time::seconds(1));
+                checkpoint();
+            }
+        }catch(timeout_exception){
+            std::cout <<"work_forever_client got timeout exception. Surprise."<<std::endl;
+            caught=true;
+        }
+    }
+};
+
+BOOST_AUTO_TEST_CASE(timeouts){
+    BOOST_CHECK_EQUAL(hub.get_n_open(), 0);
+    hub.insert_job(BSON("foo"<<1<<"bar"<<2), 1);
+    BOOST_CHECK_EQUAL(hub.get_n_open(), 1);
+    BOOST_CHECK_EQUAL(hub.get_n_assigned(), 0);
+
+    boost::asio::io_service hub_io, clt_io;
+    work_forever_client wfc("localhost","test.gtest");
+    wfc.reg(clt_io,1);
+    hub.reg(hub_io, 1);
+
+    boost::asio::deadline_timer hub_dt(hub_io, boost::posix_time::seconds(6));
+    hub_dt.async_wait(boost::bind(&boost::asio::io_service::stop, &hub_io));
+
+    boost::asio::deadline_timer clt_dt(clt_io, boost::posix_time::seconds(6));
+    clt_dt.async_wait(boost::bind(&boost::asio::io_service::stop, &clt_io));
+
+    // start client thread
+    boost::thread clt_thread(boost::bind(&boost::asio::io_service::run, &clt_io));
+    
+    hub_io.run();
+    clt_thread.join();
+
+    BOOST_CHECK(wfc.caught);
+    BOOST_CHECK_EQUAL(1, hub.get_n_failed());
 }
 BOOST_AUTO_TEST_SUITE_END()
